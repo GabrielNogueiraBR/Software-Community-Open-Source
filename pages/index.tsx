337f0ca8bdda5d7442ffeabea1968bf32a63678e
Gabriel Nogueira <gabrielnoog7@gmail.com>
2022-09-16 20:57:08 -0300

    chore: creating src folder structure

--- pages/index.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Home() {
-  return <h1>Software-Community-Open-Source P</h1>;
-}
---
+++ src/pages/_app.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { ChakraProvider } from "@chakra-ui/react";
+import { theme } from "../styles/theme";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
+}
+
+export default MyApp;

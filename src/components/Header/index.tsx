c05194a8728badcf72f21f81ebfa3bf321c083af
Vitor Gois <vitorcgois777@gmail.com>
2022-09-26 13:00:26 -0300

    feat: add header component #SCOS-42

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,47 @@
+import { Flex, Icon, IconButton, useBreakpointValue } from "@chakra-ui/react";
+import { RiMenuLine } from "react-icons/ri";
+import { useSidebarDrawer } from "../../contexts/SidebarDrawerContext";
+
+import { Logo } from "./Logo";
+import { Profile } from "./Profile";
+import { SearchBox } from "./SearchBox";
+
+export default function Header() {
+  const { onOpen } = useSidebarDrawer();
+
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  });
+
+  return (
+    <Flex
+      as="header"
+      w="100%"
+      maxWidth={1480}
+      h="20"
+      marginX="auto"
+      mt="4"
+      px="6"
+      align="center"
+    >
+      {!isWideVersion && (
+        <IconButton
+          icon={<Icon as={RiMenuLine} />}
+          fontSize="24"
+          variant="unstyled"
+          onClick={onOpen}
+          aria-label="Open navigation"
+          mr="2"
+        ></IconButton>
+      )}
+      <Logo />
+
+      {isWideVersion && <SearchBox />}
+
+      <Flex align="center" marginLeft="auto">
+        <Profile showProfileData={isWideVersion} />
+      </Flex>
+    </Flex>
+  );
+}

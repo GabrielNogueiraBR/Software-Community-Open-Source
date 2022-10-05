2f50a535dca1ca23e8eab714c5bb7eeead52610c
Gabriel Nogueira <gabrielnoog7@gmail.com>
2022-10-04 22:10:15 -0300

    feat: adding link to application Logo

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -1,22 +1,27 @@
 import { Text } from "@chakra-ui/react";
+import Link from "next/link";
 
 export function Logo() {
   return (
-    <Text
-      fontSize={["2xl", "3xl"]}
-      fontWeight={"bold"}
-      letterSpacing={"tight"}
-      w="64"
-    >
-      SCO
+    <Link href="/">
       <Text
-        as={"span"}
-        color="pink.500"
-        marginLeft="1"
-        sx={{ transform: "scaleX(-1)" }}
+        as="a"
+        cursor="pointer"
+        fontSize={["2xl", "3xl"]}
+        fontWeight={"bold"}
+        letterSpacing={"tight"}
+        w="64"
       >
-        ?
+        SCO
+        <Text
+          as={"span"}
+          color="pink.500"
+          marginLeft="1"
+          sx={{ transform: "scaleX(-1)" }}
+        >
+          ?
+        </Text>
       </Text>
-    </Text>
+    </Link>
   );
 }

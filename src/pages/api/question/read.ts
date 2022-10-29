716224fcc5fe8aca32b409bc0ca6dc9b28c0fb40
Gabriel Nogueira <gabrielnoog7@gmail.com>
2022-10-28 21:44:46 -0300

    refact: updating endpoints to use firestore service functions

--- src/pages/api/question/read.ts
+++ src/pages/api/question/read.ts
@@ -1,7 +1,12 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import firestore from "../../../services/firebase/firestore";
+
 import * as yup from "yup";
-import { Question, QuestionCategory } from "../../../types/question";
+import { QuestionCategory } from "../../../types/question";
+import { Filters } from "../../../types/filters";
+import {
+  filterQuestions,
+  findQuestionsAtFirestore,
+} from "../../../services/firebase/firestore/read";
 
 const schema = yup.object().shape({
   title: yup.string(),
@@ -9,78 +14,6 @@ const schema = yup.object().shape({
   complexity: yup.number().min(0).max(5),
   resolved: yup.boolean(),
 });
-
-type Filters = {
-  title: string;
-  category: string;
-  complexity: number;
-  resolved: boolean;
-}
-
-export const filterQuestions = (questions: Question[], filters: Filters): Question[] => {
-  const { category, complexity, resolved, title } = filters;
-
-  return questions.filter((question) =>{
-    let filterCategory = true;
-    let filterComplexity = true;
-    let filterResolved = true;
-    let filterTitle = true;
-
-    if (category) filterCategory = question.category === category;
-    if (complexity) filterComplexity = question.complexity === complexity;
-    if (resolved) filterResolved = question.resolved === resolved;
-    if (title) filterTitle = question.title.toLowerCase().includes(title.toLowerCase());
-
-    return filterCategory && filterComplexity && filterResolved && filterTitle;
-  });
-}
-
-export const findQuestionsAtFirestore = async (): Promise<Question[]> => {
-  const collectionName =
-    process.env.NEXT_PUBLIC_STAGE === "production"
-      ? "question"
-      : "question_dev";
-
-  const collectionRef = firestore.collection(collectionName);
-  const documents = await collectionRef
-    .select(
-      'assignee',
-      'title', 
-      'description', 
-      'category', 
-      'complexity', 
-      'answers', 
-      'resolved', 
-      'resolvedJustification',
-      'resolvedDate', 
-      'created', 
-      'updated', 
-      'data'
-    )
-    .get()
-    .then((res) => res.docs);
-
-  return documents.map((data) => {
-    const fields = data['_fieldsProto'];
-    const ref = data['_ref'];
-    
-    const id = ref['_path']['segments'][1]; 
-
-    const obj = {} as Question;
-    obj['id'] = id;
-
-    for (const [field, value] of Object.entries(fields)) {
-      obj[field] = value['integerValue'] 
-        ?? value['stringValue'] 
-        ?? (value['timestampValue'] && Number(value['timestampValue'].seconds * 1000))
-        ?? value['booleanValue'] 
-        ?? (value['arrayValue'] && value['arrayValue'].values)
-        ?? (value['mapValue'] && value['mapValue'].fields);
-    }
-
-    return obj;
-  });
-};
 
 export default async function handler(
   req: NextApiRequest,
@@ -102,10 +35,12 @@ export default async function handler(
 
     const { category, complexity, resolved, title } = query as any as Filters;
     const questions = await findQuestionsAtFirestore();
-    const filteredQuestions = filterQuestions(
-      questions,
-      { category, complexity, resolved, title }
-    );
+    const filteredQuestions = filterQuestions(questions, {
+      category,
+      complexity,
+      resolved,
+      title,
+    });
 
     return res.status(200).json(filteredQuestions);
   } catch (e) {
